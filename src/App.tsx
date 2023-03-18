5676441fba80ffabe933217c978b14bb292c158f
Shi Hoch <shi@scala-bio.com>
2023-03-18 16:09:44 +0200

    Got molstar to work in browser.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Molstar from "molstar-react";
 
-function App() {
+// function App() {
+//   return (
+//     <div>
+//     {/* <Molstar pdbId="1LOL"/> */}
+//     </div>
+//   );
+//     /* <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+//   */
+// }
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Molstar useInterface ="true" showControls="true" showAxes="true"/>
   );
 }
 
